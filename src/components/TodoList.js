f658f60e14e205c475124ce6eaf746129310d738
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-05-28 19:54:09 +0900

    TodoItem을 만들고 적용했다 중간에 {css} 를 해당 라이브러리에서 가져옴

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import TodoItem from './TodoItem'
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  background: gray; 
 `;
 
 function TodoList() {
   return (
   <TodoListBlock>
-      여기에 TodoItem map을 사용해서 렌더링
+    <TodoItem text="React 끝내기" done={true} />
+    <TodoItem text="인프라 구축 강의 듣기" done={true} />
+    <TodoItem text="김영한님의 Spring동영상강의 듣기" done={false}/>
   </TodoListBlock>
   );
 }
